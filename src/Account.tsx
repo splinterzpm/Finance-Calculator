27bc4c108b1a47a2bc6ada7f232ea086b61ae78a
splinterzpm <marushov1998@mail.ru>
2020-04-23 15:23:45 +0300

    Switching in app

--- src/Account.tsx
+++ src/Account.tsx
@@ -1,38 +1,73 @@
 import * as React from 'react';
 import './Account.scss';
 
-export interface AccountProps {
-    name: string
-    surname: string
-    accnumber: number;
-    
+interface Props {
+  isAdded: boolean,
+  handleClickAdd: (accName: string, balance: number) => void
+}
+
+export interface State {
+  accId: number,
+  accName: string,
+  balance: number,
+}
+
+export default class Account extends React.Component <Props, State> {
+  constructor(props: Props) {
+    super(props); 
+    this.state = { accId: 0,  accName: '1', balance: 0 }
   }
 
-export default function Account({
-    name = '',
-    surname = '',
-    accnumber,
-  }: AccountProps) {
+  handleChange = (e: any) => {
+    this.setState({ accName: e.target.value });
+  }
+
+  handleChange2 = (e: any) => {
+    this.setState({ balance: e.target.value });
+  }
+
+  render () {
+    
+    let acc = null;
+    if(!this.props.isAdded) {
+      acc = (
+        <div className="row">
+          <form>
+            <div className="row">
+              <span> Добавление счета </span>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s4">
+                <input placeholder="Account Name" type="text" className="validate"  value={this.state.accName} 
+                  onChange={this.handleChange} />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="input-field col s4">
+                <input placeholder="Balance" type="text" className="validate" value={this.state.balance} 
+                  onChange={this.handleChange2}/>
+              </div>
+              <button className="waves-effect waves-light btn cyan lighten-1 AccBtn"  
+              onClick={() => this.props.handleClickAdd(this.state.accName, this.state.balance)} > Add </button>
+            </div>
+          </form>
+        </div>
+      )
+    } else {
+      acc = (
+        <div className="AccMain">
+          <span> {this.state.accName} </span> 
+          <span> {this.state.balance} </span> 
+        </div>
+      )
+    }
     return (
-    <div className="row">
-        <form className="AccMain">
-          <div className="row">
-            <div className="input-field col s4">
-              <input placeholder="First Name" type="text" className="validate" /* value={name} 
-               onChange={handleChange()} */ />
-            </div>
-            <div className="input-field col s4">
-              <input placeholder="Last Name" type="text" className="validate" /* value={surname} 
-               onChange={handleChange2()} */ />
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-field col s4">
-              <input placeholder="Account Number" type="text" className="validate" value={accnumber} />
-            </div>
-            <a className="waves-effect waves-light btn cyan lighten-1 AccBtn" /* onClick={handleClick()} */ > Add </a>
-          </div>
-        </form>
-    </div>
+      <div>
+        {acc}
+      </div>
+        
     )
   }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.scss';
+import Transaction from './Transaction';
 import NotFound from './NotFound';
 import Home from './Home';
-import Account from './Account';
-import Transaction from './Transaction';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import Accounts from './Accounts';
+import Account from './Account';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+          <nav>
+            <div className="nav-wrapper cyan lighten-1">
+              <ul id="nav-mobile" className="left hide-on-med-and-down">
+                <li>
+                  <Link to="/" className="brand-logo right">FinCalc</Link>
+                </li>
+                <li>
+                  <Link to="/Account">Add Account</Link>
+                </li>
+                <li>
+                  <Link to="/Accounts">Accounts</Link>
+                </li>
+                <li>
+                  <Link to="/Transactions">Transactions</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div >
+          </div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Account" component={Account} />
+            <Route path="/Accounts" component={Accounts} />
+            <Route path="/Transactions" component={Transaction} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-        <nav>
-          <div className="nav-wrapper cyan lighten-1">
-            <ul id="nav-mobile" className="left hide-on-med-and-down">
-              <li>
-                <Link to="/" className="brand-logo right">FinCalc</Link>
-              </li>
-              <li>
-                <Link to="/Accounts">Accounts</Link>
-              </li>
-              <li>
-                <Link to="/Transactions">Transactions</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/Accounts" component={Account} />
-              <Route path="/Transactions" component={Transaction} />
-              <Route component={NotFound} />
-        </Switch>
-      </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+export default App;
 serviceWorker.unregister();
